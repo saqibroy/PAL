d0712e8bfdf7111c48c3e88199084ed816a8b0d7
saqibroy <saqibsohail007@gmail.com>
2016-05-03 16:50:57 +0500

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,21 @@
+
+//= require js/jquery-1.11.0.min
+//= require js/bootstrap.min
+//= require js/bootstrap-hover-dropdown.min
+//= require js/owl.carousel.min
+//= require js/jquery.parallax-1.1.3
+//= require js/jquery.nicescroll
+//= require js/jquery.prettyPhoto
+//= require js/jquery-ui-1.10.4.custom.min
+//= require js/jquery.jigowatt
+//= require js/jquery.sticky
+//= require js/waypoints.min
+//= require js/jquery.isotope.min
+//= require js/jquery.gmap.min
+//= require rs-plugin/js/jquery.themepunch.plugins.min
+//= require rs-plugin/js/jquery.themepunch.revolution.min
+//= require js/custom.js
+
+//= require jquery_ujs
+
+
